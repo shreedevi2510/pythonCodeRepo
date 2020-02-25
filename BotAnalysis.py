240b4ec871ce5891f3b333b7d7c4a0cad46401ca
Shreedevi Bugudanahalli Anandamurthy <38962818+shreedevi2510@users.noreply.github.com>
2020-02-24 22:09:35 -0500

    Add files via upload

---
+++ BotAnalysis.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  5 16:16:43 2020
+
+@author: shrik
+"""
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+#Read Data
+data_in= pd.read_excel('January Chatlog SB 3 - scrubbed.xlsx')
+
+#Plan types
+plan_types = data_in['Plan Types']
+#plan_types.dropna(inplace=True)
+plan_type_PPO=[x for x in plan_types if (x == '[PPO]')]
+plan_type_HMO=[x for x in plan_types if (x == '[HMO]')]
+plan_type_others=[x for x in plan_types if ((x !='[PPO]')&(x !='[HMO]'))]
+
+values=[len(plan_type_PPO),len(plan_type_HMO), len(plan_type_others)]
+labels=['PPO','HMO','Others']
+
+#Plot HMO vs PPO
+plt.pie(values, labels=labels)
+plt.show()
+
+#Unsupported questions
+unsupported_user_input= data_in.loc[data_in['Intent']=='unsupported.fallback']
+unsupported_user_input.to_csv('Unsupported_questions.csv')
+
+#Id card issues
+id_card_unsupported= unsupported_user_input[unsupported_user_input['User Request'].str.contains('card')]
+id_card_unsupported.to_csv('unsupported_idCard_questions.csv')
+
+#Extracting the URLs
+URL_Colmn= data_in['Quickpick URLs']
+URL_Colmn.dropna(inplace=True)
+
+
+
+  
+        
+    
+    
+    
+    
+    
+
+
+
+
+
+
