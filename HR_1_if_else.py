64730f239e27a69c3b56ba0be37bda9518a3a676
Shreedevi Bugudanahalli Anandamurthy <38962818+shreedevi2510@users.noreply.github.com>
2020-02-25 17:25:56 -0500

    Add files via upload

---
+++ HR_1_if_else.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 17:24:13 2020
+
+@author: shrik
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    w='Weird'
+    nw='Not Weird'
+
+    if n in range(1,101):
+        if((2<=n<=5)&(n%2==0)):
+            print(nw)
+        elif((6<=n<=20)&(n%2==0)):
+            print(w)
+        elif((n>20)&(n%2==0)):
+            print(nw)
+        elif(n%2!=0):
+            print(w)
+    
