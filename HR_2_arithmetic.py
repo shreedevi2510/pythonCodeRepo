8f451d22f3eeb814d317db130d127ff2a13a930f
Shreedevi Bugudanahalli Anandamurthy <38962818+shreedevi2510@users.noreply.github.com>
2020-02-25 17:29:31 -0500

    Add files via upload

---
+++ HR_2_arithmetic.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 17:26:55 2020
+
+@author: shrik
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    if((1<=a<=10**10)&(1<=b<=10**10)):
+        print(a+b)
+        print(a-b)
+        print(a*b)
---
+++ HR_3_Div.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 17:28:00 2020
+
+@author: shrik
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    if(b!=0):
+        print(a//b)
+        print(a/b)
